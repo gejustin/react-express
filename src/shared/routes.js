c87e03cc394b6e96614249605b436b0449ee9167
gary.justin <gary.justin@forrent.com>
2015-09-10 22:22:46 -0400

    Initial commit

---
+++ src/shared/routes.js
@@ -0,0 +1,8 @@
+﻿import { Route } from "react-router";
+import React from "react";
+
+import AppHandler from "./components/AppHandler";
+
+export default (
+  <Route handler={ AppHandler } path="/" />
+);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+    context: __dirname,
+    entry: './client/index.js',
+    output: {
+        path: __dirname + '/client',
+        filename: 'bundle.js'
+    }
+};
